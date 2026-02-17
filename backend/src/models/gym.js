ab81b806a1ae768e778c9f358b45132c905bc1d5
Kush <kush62831@gmail.com>
2026-02-17 18:25:13 +0530

    gym schema and validation

---
+++ backend/src/models/gym.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const gymSchema = new mongoose.Schema({
+    Name:{
+        type:String,
+        required:true,
+        minLength:[5,`Too short Name`],
+        maxLenght:20
+    },
+    EmailId:{
+        type:String,
+        required:true,
+        index:true,
+        unique:true,
+        lowercase:true,
+        validate(value){
+         if(!validator.isEmail(value)){
+            throw new Error("Invalid E-mail Id");
+         }
+        }
+    },
+    Password:{
+      type:String,
+      required:true,
+      trim:true,
+      validate(value){
+        if(!validator.isStrongPassword){
+            throw new Error("Invalid Password")
+        }
+      }
+
+    },
+    PhoneNumber:{
+        type:String,
+        required:true,
+        validate(value){
+            if(!validator.isMobilePhone(value)){
+                throw new Error("Invalid Phone Number")
+            }
+        }
+    },
+    Address:{
+        type:String,
+        required:true,
+        minLength:[12,'Not enough characters'],
+        maxLength:100,
+    }
+},{
+    timestamps:true
+});
+module.exports = mongoose.model("Gym",gymSchema);
