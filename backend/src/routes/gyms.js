4bc032514587342a34609c050eb5fdf987e8fd06
Kush <kush62831@gmail.com>
2026-02-23 01:25:48 +0530

    made API for active trainers and clients

--- backend/src/routes/gyms.js
+++ backend/src/routes/gyms.js
@@ -1,15 +1,16 @@
 const express = require("express");
+const gymRouter = express.Router();
 const { authorizeRoles } = require("../middlewares/authorizeRoles");
 const {userAuth} = require("../middlewares/userAuth");
 const Gym = require("../models/gym");
-const gymRouter = express.Router();
 
 
-gymRouter.get("/gyms/view", userAuth,authorizeRoles, async (req,res) => {
+
+gymRouter.get("/gyms/view", userAuth,authorizeRoles("owner"), async (req,res) => {
     try{
-      const user = req.user;  
-      const gym = await Gym.findById({
-        ownerId:user._id
+      const owner = req.user;  
+      const gym = await Gym.findOne({
+        ownerId:owner._id
       });
       res.status(200).json({
         message:"Gym data fetch Successfully",gym
@@ -17,7 +18,7 @@ gymRouter.get("/gyms/view", userAuth,authorizeRoles, async (req,res) => {
     }catch(err){
         res.status(404).json("Error :" + err.message);
     }
-})
+});
 
 
 module.exports = gymRouter;
